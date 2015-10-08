55fa25a7439270f078c482228786119fa3c028d7
CoreOS Admin <you@example.com>
2015-10-08 05:43:10 +0000

    changed server to prod mode

--- Readme.md
+++ Readme.md
@@ -5,5 +5,5 @@ docker run -p 80:80 --name xpika_symfony_demo -i -t -d xpika/docker-symfony-demo
 ```
 
 Paths
-* /get_cms
-* /set_cms
+* http://localhost/get_cms
+* http://localhost/set_cms
--- workdir/entrypoint.sh
+++ workdir/entrypoint.sh
@@ -1,4 +1,4 @@
 sh /root/workdir/copyfiles.sh
 php /root/project/app/console doctrine:fixtures:load --no-interaction
-php /root/project/app/console server:start 0.0.0.0:80
+php /root/project/app/console server:start --env=prod 0.0.0.0:80
 /bin/bash
